c30d14748ac2df10db253082b98c55b7052cd31e
Silas Santos <sillassanttos@outlook.com>
2023-03-05 00:21:43 -0400

    Populando tabelas com relacionamento muitos para muitos

--- Script.sql
+++ Script.sql
@@ -74,3 +74,15 @@ create table tb_pedidos_produtos(
 	foreign key(id_pedido) references tb_pedidos(id_pedido),
 	foreign key(id_produto) references tb_produtos(id_produto)
 );
+
+INSERT INTO tb_clientes(nome, idade) VALUES('Jorge', 29);	
+INSERT INTO tb_pedidos(id_cliente) VALUES(1);	
+INSERT INTO tb_pedidos_produtos(id_pedido, id_produto) VALUES(1, 2);
+INSERT INTO tb_pedidos_produtos(id_pedido, id_produto) VALUES(1, 3);
+INSERT INTO tb_pedidos(id_cliente) VALUES(1);
+INSERT INTO tb_pedidos_produtos(id_pedido, id_produto) VALUES(2,3);
+INSERT INTO tb_clientes(nome, idade) VALUES('Jamilton', 30);
+INSERT INTO tb_pedidos(id_cliente) VALUES(2);
+INSERT INTO tb_pedidos_produtos(id_pedido, id_produto) VALUES(3,1);
+
+
