a8f8737354f251743c86e87c95221f38d457fc47
Silas Santos <sillassanttos@outlook.com>
2023-03-04 23:56:51 -0400

    Adicionado tabelas para relacionamento 1 para 1

--- Script.sql
+++ Script.sql
@@ -1,1 +1,30 @@
-a
+create database lojavirtual;
+
+use lojavirtual;
+
+/* relacionamento Um para Um
+ * 
+ * 
+ *   tb_produtos
+ *   tb_descricoes_tecnicas
+ * 
+ *  */
+
+create table tb_produtos(
+	id_produto int not null primary key auto_increment,
+	produto varchar(200) not null,
+	valor float(8,2) not null default 0
+);
+
+create table tb_descricoes_tecnicas(
+	id_descricao_tecnica int not null primary key auto_increment,
+	id_produto int not null,
+	descricao_tecnica text not null,
+	foreign key(id_produto) references tb_produtos(id_produto)
+);
+
+
+
+
+
+
